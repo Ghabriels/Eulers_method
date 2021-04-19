dff4c9fa5eee65b1f605e9ba921145483f42ce0f
Ghabriels <82556497+Ghabriels@users.noreply.github.com>
2021-04-19 15:09:47 -0300

    Update Mt_Euler.R
    Add some labels.

--- Mt_Euler.R
+++ Mt_Euler.R
@@ -1,16 +1,23 @@
+## Euler's Method for Linear 
 
-
+#Domain
 x<-seq(1,3,0.4)
+#Ordinary Differential Equation
 F<-function(x,y) (1+(y/x)+(y/x)^2)
 y=0
+#Euler's Approximations
 aprox.<-seq(0,0,0)
 for(i in 1:5) {y=y+0.4*F(x[i],y); aprox.[i+1]<-y} 
-f<-function(x) (tan(log(x, base=exp(1)))/x)
+#Exact Solution
+f<-function(x) (tan(log(x, base=exp(1)))/x)  
 exata<-seq(0,0,0)
 for(i in 1:6) {t=f(x[i]); exata[i]<-t}
+#Error 
 erro.absoluto<-seq(0,0,0)
 for(i in 1:6) {s=aprox.[i]-exata[i]; erro.absoluto[i]<-s}
+#Comparation Data Frame
 comparacao<-cbind(x,aprox.,exata,erro.absoluto)
 comparacao
 mean(erro.absoluto)
+#Approximations Plot
 plot(erro.absoluto, type = "b")
